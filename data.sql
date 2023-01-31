973990d0d606b0318df870bb768503cee75987b7
girma <girmatarekegn.gi@gmail.com>
2023-01-31 09:07:59 +0300

    Add updated data.sql add new value to animals table

--- data.sql
+++ data.sql
@@ -1,10 +1,35 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-    1, 'Agumon', '2020-02-03', 0, true, 10.23);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-    2, 'Gabumon', '2018-11-03', 2, true, 8);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-    3, 'Pikachu', '2021-01-07', 1, false, 15.04);
- INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-    4, 'Devimon', '2017-05-12', 5, true, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
+    'Agumon', '2020-02-03', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Gabumon', '2018-11-03', 2, true, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Pikachu', '2021-01-07', 1, false, 15.04);
+ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Devimon', '2017-05-12', 5, true, 11);
+
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
+    'Charmander', '2020-02-08', 0, false, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Plantmon', '2021-11-15', 2, true, 5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Squirtle', '1993-04-02', 3, false, 12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Angemon', '2005-06-12', 1, true, 45);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Boarmon', '2005-6-7', 7, true, 20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Blossom', '1998-10-13', 3, true, 17);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+    'Ditto', '2022-5-14', 4, true, 22);
+
+-- Set a value 'digimon' for animals to column species where name end with mon string
+
+UPDATE animals SET species = 'digimon' WHERE NAME LIKE '%mon';
+
+-- Set a value 'pokemon' for animals table to column species 
+
+UPDATE animals SET species = 'pokemon' WHERE NAME  NOT LIKE '%mon';
+
